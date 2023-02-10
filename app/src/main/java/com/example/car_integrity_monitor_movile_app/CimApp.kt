0171453246e415eeefcaf5037da8e211baa0b43d
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2023-02-09 20:59:27 -0500

    TopBar and BottomBar is showing, first screen button showing likewise

--- app/src/main/java/com/example/car_integrity_monitor_movile_app/CimApp.kt
+++ app/src/main/java/com/example/car_integrity_monitor_movile_app/CimApp.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.car_integrity_monitor_movile_app.bottomnav.BottomNavItem
 import com.example.car_integrity_monitor_movile_app.ui.screens.CarStateScreen
 
 
@@ -14,7 +17,7 @@ fun CimAppBar(
     modifier: Modifier = Modifier
 ){
     TopAppBar(
-        title = { Text("Titulo") },
+        title = { Text("App Name") },
         modifier = modifier,
         navigationIcon = {
             IconButton(onClick = { /*TODO*/ }) {
@@ -27,15 +30,41 @@ fun CimAppBar(
     )
 }
 
+@Composable
+fun CimBottomNavigationBar(
+    modifier: Modifier = Modifier
+){
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf(
+        BottomNavItem.CarState,
+        BottomNavItem.CarAnomaly,
+        BottomNavItem.Notification
+    )
 
+    BottomNavigation {
+        items.forEachIndexed{index, item ->
+            BottomNavigationItem(
+                icon = { Icon(
+                    painter = painterResource(id = item.icon),
+                    contentDescription = stringResource(id = item.title)
+                )},
+                label = { Text(text = stringResource(id = item.title)) },
+                selected = selectedItem == index,
+                onClick = { /*TODO*/ }
+            )
+        }
+    }
+}
 
 @Composable
 fun CimApp(
     modifier: Modifier = Modifier
 ){
-    Scaffold(topBar = { CimAppBar()}) {
+    Scaffold(
+        topBar = { CimAppBar() },
+        bottomBar = { CimBottomNavigationBar() }
+    ) {
         CarStateScreen()
-
     }
 }
 
